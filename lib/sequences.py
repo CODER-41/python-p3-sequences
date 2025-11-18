f6a07e1867539f3f18d5e76a3f95c7f9a5eb57a3
RONNY <mboyaronny41@gmail.com>
2025-11-18 14:43:54 +0300

    sequence

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+
+    if not isinstance(length, int) or length < 0:
+        print("Error: Length must be a non-negative integer.")
+        return
+    
+    if length == 0:
+        fib_list = []
+    elif length == 1:
+        fib_list = [0]
+    else:
+        #start with the first two numbers of the sequence
+        fib_list = [0, 1]
+        while len(fib_list) < length:
+            next_num = fib_list[-1] + fib_list[-2]
+            fib_list.append(next_num)
+
+    print(fib_list)
+
+
+    
+
